6f9bb79e34c07a8eaf2875012884eb9ea6a730d6
eric <bullship15@gmail.com>
2022-09-09 07:38:16 -0700

    Task 5 to fix error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success
+ * main - Prints a text
+ *
+ * Return: Always 0 (success)
  * */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n"
-);
+	printf("with proper grammar, but the outcome is a piece of 
+art,\n");
 	return (0);
 }
