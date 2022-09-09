8bec233b9f8031380eb26220593df231f950db22
eric <bullship15@gmail.com>
2022-09-09 06:03:50 -0700

    Hello printf fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - print the string in the put function
+ * main - entry point
  *
- * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle
- * Return: 0
+ * Return: 0 after printing the function
  */
 int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-
+{printf("\"with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
