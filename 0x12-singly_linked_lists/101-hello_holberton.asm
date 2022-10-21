b788116cb33761504d49151fc4a257e2b38f666b
eric <bullship15@gmail.com>
2022-10-20 23:28:50 -0700

    Task 6 fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,26 @@
-#include <stdio.h>
+; File: 101-hello_holberton.asm
+; Auth: Gedeon Obae Gekonge
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
 
-void __attribute__((constructor)) hare(void);
+extern printf
 
-/**
- * hare - Prints a string before the
- *        main function is executed.
- */
-void hare(void)
-{
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
-}
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
